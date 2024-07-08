27cc482d8083280b51cda33b1173d72e0b5aaa3d
Jeremy <jeremygprawira@gmail.com>
2024-07-08 23:52:25 +0700

    feat: add comment and share link button

--- components/PostActionBar.tsx
+++ components/PostActionBar.tsx
@@ -2,6 +2,10 @@ import { PostWithExtras } from "@/lib/definitions";
 import { cn } from "@/lib/utils";
 import React from "react";
 import LikeButton from "./Like";
+import Link from "next/link";
+import ActionIcon from "./ActionIcon";
+import { MessageCircle } from "lucide-react";
+import ShareButton from "./ShareButton";
 
 function PostActionBar({
   post,
@@ -15,6 +19,12 @@ function PostActionBar({
   return (
     <div className={cn("relative flex items-center w-full gap-x-2", className)}>
       <LikeButton post={post} userId={userId} />
+      <Link href={`/dashboard/p/${post.id}`}>
+        <ActionIcon>
+          <MessageCircle className={"h-6 w-6"}/>
+        </ActionIcon>
+      </Link>
+      <ShareButton postId={post.id}/>
     </div>
   );
 }
